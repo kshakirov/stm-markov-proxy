cbf5a22cbbc5e833fc66e7d6e7bbd762cad6d074
Kirill Shakirov <kirill.shakirov4@gmail.com>
2026-07-17 08:45:53 +0300

    Socket added

--- app/Main.hs
+++ app/Main.hs
@@ -2,9 +2,12 @@
 
 module Main where
 
-import Control.Concurrent.STM (TVar, newTVarIO, readTVar, writeTVar)
+import Control.Concurrent.STM (TVar, newTVarIO, readTVar, writeTVar, STM,atomically)
 import Control.Monad.Reader
 import qualified Control.Monad.Trans.Reader as RT
+import Network.Socket (Socket)
+import qualified Network.Socket as S
+
 import qualified MyLib (someFunc)
 
 data Env = Env
@@ -20,7 +23,8 @@ data BackendConfig = BackendConfig
 
 data Config = Config
   { hostName :: String,
-    port :: Int
+    port :: Int,
+    backends ::Int
   }
 
 data ProxyState = ProxyState
@@ -34,6 +38,10 @@ listenAndServe = do
   --  let c = Config{hostName="proxyHost", port=8989, backends = [1,2]}
   env <- ask
   let name = (hostName . proxyConfig) env
+  let ends =( backends  . proxyConfig)env
+  let tVarState = proxyTVarState env
+  sb <- liftIO $ atomically $ nextBackendIdxTx tVarState ends
+  liftIO $ putStrLn  (show sb)
   --  liftIO $  putStrLn $ hostName c
   liftIO $ putStrLn $ "The host is " ++ name
 
@@ -44,7 +52,40 @@ main = do
   putStrLn "Hello, Haskell!"
   let initProxyState = ProxyState {nextBackendIndex = 0}
   refProxyTVarState <- newTVarIO initProxyState
-  let c = Config {hostName = "localhost", port = 8989}
+  let c = Config {hostName = "localhost", port = 8989, backends=3}
 
   let e = Env {proxyConfig = c, backendConfigs = [], proxyTVarState = refProxyTVarState}
   runReaderT listenAndServe e
+
+
+-- Нам понадобятся функции readTVar и writeTVar из Control.Concurrent.STM
+nextBackendIdxTx :: TVar ProxyState -> Int -> STM Int
+nextBackendIdxTx stateRef totalBackends = do
+  -- 1. Читаем текущее состояние из транзакционной переменной
+  currentState <- readTVar stateRef
+  
+  -- 2. Достаем текущий индекс
+  let currentIdx = nextBackendIndex currentState
+  
+  -- 3. Вычисляем следующий индекс по формуле Round-Robin
+  -- Если бэкендов 0, то индекс всегда 0, чтобы избежать деления на ноль
+  let nextIdx = if totalBackends == 0 
+                then 0 
+                else (currentIdx + 1) `mod` totalBackends
+                
+  -- 4. Записываем обновленное состояние обратно
+  writeTVar stateRef (ProxyState { nextBackendIndex = nextIdx })
+  
+  -- 5. Возвращаем ТЕКУЩИЙ индекс, по которому прокси должен отправить запрос
+  return currentIdx
+
+
+openListeningSocket :: Int -> IO Socket
+openListeningSocket portNum = do
+  let hints = S.defaultHints { S.addrFlags = [S.AI_PASSIVE], S.addrSocketType = S.Stream }
+  addrInfo <- head <$> S.getAddrInfo (Just hints) Nothing (Just (show portNum))
+  sock <- S.socket (S.AF_INET) (S.addrSocketType addrInfo) (S.addrProtocol addrInfo)
+  S.setSocketOption sock S.ReuseAddr 1
+  S.bind sock (S.addrAddress addrInfo)
+  S.listen sock 1024
+  return sock
