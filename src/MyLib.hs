2e494bbf912eec84b12ebcda2c8104307561cf4e
Kirill Shakirov <kirill.shakirov4@gmail.com>
2026-07-29 21:20:03 +0300

    started markov 2

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,12 +1,19 @@
 {-# LANGUAGE OverloadedStrings #-}
 module MyLib (someFunc) where
-import Data.ByteString (ByteString)
+import Data.ByteString (ByteString, uncons, breakSubstring)
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
-runMarkov :: ByteString -> ByteString
-runMarkov s = s
-
 runMarkovStep :: ByteString -> ByteString
-runMarkovStep s =s 
+runMarkovStep s =
+  let (before, after) = breakSubstring  "fd" s 
+      result = after
+  in result 
+
+
+
+runMarkov :: ByteString -> ByteString
+runMarkov  s =  case uncons s of
+  Nothing -> ""
+  Just (x,xs) -> runMarkovStep xs
