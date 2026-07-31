0f22d40ad161db71790032110f550a31dd38d87c
Kirill Shakirov <kirill.shakirov4@gmail.com>
2026-07-31 08:21:00 +0300

    Markov wip 2

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -7,15 +7,23 @@ import qualified Data.ByteString as B
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
-runMarkovStep :: B.ByteString -> B.ByteString ->B.ByteString  -> B.ByteString
+runMarkovStep :: B.ByteString -> B.ByteString ->B.ByteString  -> (B.ByteString,  Bool)
 runMarkovStep s t r  =
   let (before, after) = B.breakSubstring  t s
-      result = if (B.null after) then before else  B.concat [before, r,  B.drop (B.length t) after]
+      result = if (B.null after) then (before, False)  else  (B.concat [before, r,  B.drop (B.length t) after], True)
   in result
 
 
 
-runMarkov :: B.ByteString -> B.ByteString
-runMarkov  s =  case B.uncons s of
-  Nothing -> ""
-  Just (x,xs) -> xs
+runMarkov :: [(B.ByteString, B.ByteString)] -> B.ByteString ->B.ByteString
+runMarkov  [] s = s
+runMarkov (r:rules)   s =
+  let transformed = runMarkovStep s (fst r) (snd r)
+  in case transformed of
+        (ts, True) -> runMarkov (r:rules) ts
+        (ts, False) -> runMarkov rules s
+ 
+       
+  
+  
+
