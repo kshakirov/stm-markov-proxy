d5ccc4ba31b916f4fc655c723604990092c0ea18
Kirill Shakirov <kirill.shakirov4@gmail.com>
2026-07-31 07:42:14 +0300

    Markov wip 1

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings #-}
 module MyLib (someFunc) where
 import qualified  Data.ByteString as B -- (ByteString, uncons, breakSubstring, concat, dro
 import qualified Data.ByteString as B
@@ -9,8 +10,8 @@ someFunc = putStrLn "someFunc"
 runMarkovStep :: B.ByteString -> B.ByteString ->B.ByteString  -> B.ByteString
 runMarkovStep s t r  =
   let (before, after) = B.breakSubstring  t s
-      tail = B.drop 2 after
-  in B.concat [before, r, tail]
+      result = if (B.null after) then before else  B.concat [before, r,  B.drop (B.length t) after]
+  in result
 
 
 
