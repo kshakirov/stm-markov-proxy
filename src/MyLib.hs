90d78dacf7a1aeda509f369fa48ed47adc747649
Kirill Shakirov <kirill.shakirov4@gmail.com>
2026-08-05 20:55:04 +0300

    Markov wip 4

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,8 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE OverloadedStrings #-}
+
 module MyLib (someFunc) where
 import qualified  Data.ByteString as B -- (ByteString, uncons, breakSubstring, concat, dro
-import qualified Data.ByteString as B
+
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
@@ -15,15 +15,18 @@ runMarkovStep s t r  =
 
 
 
+
 runMarkov :: [(B.ByteString, B.ByteString)] -> B.ByteString ->B.ByteString
 runMarkov  [] s = s
-runMarkov allRules@(r:rules)   s =
-  let transformed = runMarkovStep s (fst r) (snd r)
-  in case transformed of
-        (ts, True) -> runMarkov allRules ts
-        (ts, False) -> runMarkov rules s
+runMarkov allRules   s = go allRules s where
+  go [] s = s
+  go (r:rs ) s = 
+    let transformed = runMarkovStep s (fst r) (snd r)
+    in case transformed of
+         (ts, True) -> runMarkov allRules ts
+         (ts, False) -> go rs ts
  
-       
+  
   
   
 
