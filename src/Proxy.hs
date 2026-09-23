edf35a6ae661395083ef1c2f840401fe3702f6a0
Kirill Shakirov <kirill.shakirov4@gmail.com>
2026-09-23 21:29:48 +0300

    wip 3

--- src/Proxy.hs
+++ src/Proxy.hs
@@ -6,22 +6,8 @@ module Proxy (runMarkov, requestStreamAutomaton, RequestStreamAutomatonStatus(..
 import qualified Data.ByteString as B 
 import Data.Word
 import Proxy.Types
+import Proxy.Markov
 
-runMarkovStep :: B.ByteString -> B.ByteString -> B.ByteString -> (B.ByteString, Bool)
-runMarkovStep s t r =
-  let (before, after) = B.breakSubstring t s
-      result = if (B.null after) then (before, False) else (B.concat [before, r, B.drop (B.length t) after], True)
-   in result
-
-runMarkov :: [(B.ByteString, B.ByteString)] -> B.ByteString -> B.ByteString
-runMarkov allRules s = go allRules s
-  where
-    go [] ss = ss
-    go (r : rs) ss =
-      let transformed = uncurry (runMarkovStep ss) r
-       in case transformed of
-            (ts, True) -> runMarkov allRules ts
-            (ts, False) -> go rs ts
 
 runWirth :: ParserState -> B.ByteString -> ParserState
 runWirth s b = case currentState s of
